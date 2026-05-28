adac4c20eff75c358c6fed422facacc0dc542559
Pedro Barros <barrospedro500@gmail.com>
2026-05-28 00:15:32 -0400

    feat: add bug

--- src/gc_api/main.py
+++ src/gc_api/main.py
@@ -24,7 +24,7 @@ def convert_celsius_to_fahrenheit(value: float):
 
 def main():
 
-    
+    raise Exception("Fail")
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--mode", default="development", choices=["development", "production"])
